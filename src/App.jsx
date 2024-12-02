16eee25432b3385c3095d9a51539a4dbe9e26180
GmMamunH <gmmamun3004@gmail.com>
2024-12-02 15:32:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-2xl text-center text-red-500 font-bold underline underline-offset-2 p-5">Install ReactJS And TailwindCSS Using Vite </h1>
+    </>
+  )
+}
+
+export default App
